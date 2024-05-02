a795f7f140af75781a65c25dcc677a6e2d69e95d
Oh Jeong Hun <ojh650765@gmail.com>
2024-05-02 13:40:02 +0900

    변수 이름 변경

--- Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/pixelgame/game/Bomb.java
+++ Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/pixelgame/game/Bomb.java
@@ -7,13 +7,13 @@ import kr.ac.tukorea.ge.spg.ojh.framework.objects.Sprite;
 import kr.ac.tukorea.ge.spg.ojh.pixelgame.R;
 
 public class Bomb  extends Sprite implements IBoxCollidable {
-    private static final float OBSTACLE_WIDTH = 0.6f;
-    private static final float OBSTACLE_HEIGHT = OBSTACLE_WIDTH ;
+    private static final float BOMB_WIDTH = 0.55f;
+    private static final float BOMB_HEIGHT = BOMB_WIDTH ;
 
 
     public Bomb(float x, float y) {
         super(R.mipmap.bomb);
-        setPosition(x, y, OBSTACLE_WIDTH, OBSTACLE_HEIGHT);
+        setPosition(x, y, BOMB_WIDTH, BOMB_HEIGHT);
     }
 
     @Override

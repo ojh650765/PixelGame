aee235026ade9545bf12665b8b50c58842677be3
JeongHun <45549589+ojh6507@users.noreply.github.com>
2024-05-04 03:36:24 +0900

    보드 비율 변경

--- Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/pixelgame/game/Board.java
+++ Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/pixelgame/game/Board.java
@@ -15,13 +15,13 @@ public class Board extends Sprite {
 
     protected MainScene.Layer layer;
     private static final float BOARD_WIDTH = 6.f;
-    private static final float BOARD_HEIGHT = BOARD_WIDTH;
+    private static final float BOARD_HEIGHT = BOARD_WIDTH*816/800;
     private final Random random = new Random();
 
     protected Board(MainScene.Layer layer) {
         super(R.mipmap.tile1);
         this.layer = layer;
-        setPosition(Metrics.width/2, Metrics.height/2 +1.5f ,BOARD_WIDTH, BOARD_HEIGHT);
+        setPosition(Metrics.width/2, Metrics.height/2 +1.44f ,BOARD_WIDTH, BOARD_HEIGHT);
     }
     @NonNull
     @Override

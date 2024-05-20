446479486e4bbcd35e08a34c108e1cbcb3a16d17
Oh Jeong Hun <ojh650765@gmail.com>
2024-05-20 11:37:11 +0900

    Update EnemyGenerator.java

--- Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/pixelgame/game/EnemyGenerator.java
+++ Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/pixelgame/game/EnemyGenerator.java
@@ -6,15 +6,15 @@ import android.util.Log;
 import java.util.Random;
 
 import kr.ac.tukorea.ge.spg.ojh.framework.interfaces.IGameObject;
-import kr.ac.tukorea.ge.spg.ojh.framework.scene.Scene;
+import kr.ac.tukorea.ge.spg.ojh.pixelgame.game.MainScene;
 
 public class EnemyGenerator implements IGameObject {
     private static final String TAG = EnemyGenerator.class.getSimpleName();
-
-    private int wave=0;
-    private  Scene scene;
-    public EnemyGenerator(Scene scene){
-        this.scene =scene;
+    private  MainScene scene;
+    private int stage;
+    public EnemyGenerator(MainScene scene){
+        this.scene = scene;
+        this.stage = scene.getStage();
         generate();
     }
     @Override
@@ -24,7 +24,8 @@ public class EnemyGenerator implements IGameObject {
     private void generate() {
         if (scene == null) return;
         for (int i = 0; i < 3; i++) {
-            scene.add(MainScene.Layer.enemy, Slime.get(i));
+            scene.add(MainScene.Layer.enemy, Slime.get(stage, i));
+
         }
     }
 

046023006213344c00be2cde1928043710efdd10
JeongHun <45549589+ojh6507@users.noreply.github.com>
2024-06-04 06:17:51 +0900

    숫자 배치를 편하게 하기 위해서 수정

--- Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/framework/objects/Score.java
+++ Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/framework/objects/Score.java
@@ -7,25 +7,40 @@ import android.graphics.RectF;
 
 import kr.ac.tukorea.ge.spg.ojh.framework.interfaces.IGameObject;
 import kr.ac.tukorea.ge.spg.ojh.framework.res.BitmapPool;
+import kr.ac.tukorea.ge.spg.ojh.framework.view.Metrics;
 
 public class Score implements IGameObject {
     private final Bitmap bitmap;
-    private final float right, top, dstCharWidth, dstCharHeight;
     private final Rect srcRect = new Rect();
     private final RectF dstRect = new RectF();
     private final int srcCharWidth, srcCharHeight;
-    private int score, displayScore;
+    private float right, top, dstCharWidth, dstCharHeight;
+    private int score;
 
-    public Score(int mipmapId, float right, float top, float width) {
+    private int displayScore;
+
+    private static final float DEFAULT_SCORE_WIDTH = 0.18f;
+    private static final float DEFAULT_SCORE_HEIGHT = DEFAULT_SCORE_WIDTH * 240 / 70;
+
+    public Score(int mipmapId) {
         this.bitmap = BitmapPool.get(mipmapId);
+        this.srcCharWidth = bitmap.getWidth() / 10;
+        this.srcCharHeight = bitmap.getHeight();
+    }
+
+    public void setPosition(float x, float y) {
+        setPosition(x, y, DEFAULT_SCORE_WIDTH, DEFAULT_SCORE_HEIGHT);
+
+    }
+
+    public void setPosition(float right, float top, float width, float height) {
         this.right = right;
         this.top = top;
         this.dstCharWidth = width;
-        this.srcCharWidth = bitmap.getWidth() / 10;
-        this.srcCharHeight = bitmap.getHeight();
-        this.dstCharHeight = dstCharWidth * srcCharHeight / srcCharWidth;
-    }
+        this.dstCharHeight = height;
 
+        dstRect.set(right - width / 2, top - height / 2, right + width / 2, top + height / 2);
+    }
     public void setScore(int score) {
         this.score = this.displayScore = score;
     }
@@ -47,6 +62,13 @@ public class Score implements IGameObject {
     public void draw(Canvas canvas) {
         int value = this.displayScore;
         float x = right;
+        if (value == 0) {
+            int digit = 0; // 숫자 0
+            srcRect.set(digit * srcCharWidth, 0, (digit + 1) * srcCharWidth, srcCharHeight);
+            x -= dstCharWidth;
+            dstRect.set(x, top, x + dstCharWidth, top + dstCharHeight);
+            canvas.drawBitmap(bitmap, srcRect, dstRect, null);
+        }
         while (value > 0) {
             int digit = value % 10;
             srcRect.set(digit * srcCharWidth, 0, (digit + 1) * srcCharWidth, srcCharHeight);

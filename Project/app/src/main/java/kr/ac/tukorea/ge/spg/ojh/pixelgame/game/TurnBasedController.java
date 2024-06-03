b879307820feb8cc1912f73f2ed69b3069d31746
JeongHun <45549589+ojh6507@users.noreply.github.com>
2024-06-04 06:20:21 +0900

    너무 빠른 씬전환, 빠른 보드판 리셋이  마음에 안든다. 딜레이를 넣자

--- Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/pixelgame/game/TurnBasedController.java
+++ Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/pixelgame/game/TurnBasedController.java
@@ -11,14 +11,19 @@ import kr.ac.tukorea.ge.spg.ojh.pixelgame.game.GameOverScene;
 import kr.ac.tukorea.ge.spg.ojh.framework.util.CollisionHelper;
 
 public class TurnBasedController implements IGameObject {
+
     private static final String TAG = TurnBasedController.class.getSimpleName();
     private final MainScene scene;
     private final WarriorHead warriorHead;
     private final Warrior warrior;
     private TileGenerator tileGenerator;
     private float fResetdelayTime;
+    private float fClearScenedelayTime;
     private float fAccumulateResetTime;
+    private float fAccumulateClearSceneTime;
     private boolean resetTurn = false;
+    private float fSlimeDelayTime;
+    private float fAccumulateSlimeTime;
 
     public TurnBasedController(MainScene scene, TileGenerator tileGenerator, WarriorHead warriorHead, Warrior warrior) {
         this.scene = scene;
@@ -27,7 +32,11 @@ public class TurnBasedController implements IGameObject {
         this.warrior = warrior;
 
         this.fResetdelayTime= 1.3f;
+        this.fClearScenedelayTime= 1.f;
         this.fAccumulateResetTime= 0;
+        this.fAccumulateClearSceneTime= 0;
+        this.fSlimeDelayTime= 0.8f;
+        this.fAccumulateSlimeTime= 0.f;
         this.resetTurn =false;
     }
     @Override
@@ -36,36 +45,89 @@ public class TurnBasedController implements IGameObject {
 
         if(resetTurn){
             fAccumulateResetTime+=elapsedSeconds;
-            if( fAccumulateResetTime >= fResetdelayTime){
+            if(fAccumulateResetTime >= fResetdelayTime){
                 fAccumulateResetTime = 0;
                 resetTurn = false;
+                warriorHead.ResetDef();
+                scene.SetDefScore(0);
                 ResetWarriorAndTiles();
+                this.tileGenerator.ResetGenerateObjects(warriorHead);
+                GameStateManager.getInstance().setTurnActive(false);
             }
         }
         else if(GameStateManager.getInstance().isTurnActive()) {
             warrior.ChangeState(Warrior.State.attack);
-            ArrayList<IGameObject> Slimes = scene.objectsAt(MainScene.Layer.enemy);
-            for (int s = Slimes.size() - 1; s >= 0; s--) {
-                Slime slime = (Slime) Slimes.get(s);
-                slime.startLeftMove(slime.getMoveDistance());
-                if(slime.GetAttackStats()){
-                    warriorHead.GetDamage(slime.GetPower());
+            fAccumulateSlimeTime+=elapsedSeconds;
+            if(fAccumulateSlimeTime >= fSlimeDelayTime) {
+                fAccumulateSlimeTime = 0;
+                ArrayList<IGameObject> Slimes = scene.objectsAt(MainScene.Layer.enemy);
+                for (int s = Slimes.size() - 1; s >= 0; s--) {
+                    Slime slime = (Slime) Slimes.get(s);
+                    slime.startLeftMove(slime.getMoveDistance());
+
+                    if (slime.GetAttackStats()) {
+                        warriorHead.GetDamage(slime.GetPower());
+                        warrior.ChangeHittedState();
+                        warriorHead.ResetDef();
+
+                    }
                 }
+                resetTurn = true;
             }
-            resetTurn = true;
         }
-        if(warriorHead.GetHP()<= 0){
-            new GameOverScene().push();
+       if (CheckMonsterState(elapsedSeconds) == false){
+            if(warriorHead.GetHP()<= 0){
+                new GameOverScene().push();
+            }
+       }
+    }
+    private  boolean CheckMonsterState(float elapsedSeconds) {
+        int count = 0;
+        ArrayList<IGameObject> Slimes = scene.objectsAt(MainScene.Layer.enemy);
+        for (int s = Slimes.size() - 1; s >= 0; s--) {
+            Slime slime = (Slime) Slimes.get(s);
+            if (slime.dead) {
+                count += 1;
+            } else {
+                count = 0;
+                break;
+            }
         }
+        if (count >= Slimes.size()) {
+            fAccumulateClearSceneTime+=elapsedSeconds;
+            if (fAccumulateClearSceneTime >= fClearScenedelayTime) {
+                fAccumulateClearSceneTime = 0;
+                GameStateManager gameStateManager = GameStateManager.getInstance();
+                float pw = gameStateManager.GetPowerInfo();
+                float def = gameStateManager.GetDefInfo();
+
+                gameStateManager.UpdatePowerInfo(pw);
+                gameStateManager.UpdateDefInfo(def);
+
+                float totalAtk = pw +10;
+                float totalDef = def +10;
+                if(totalAtk>=100) totalAtk =99;
+                if(totalDef>=100) totalDef =99;
+
+                gameStateManager.UpdatePowerInfo(totalAtk);
+                gameStateManager.UpdateDefInfo(totalDef);
+
+                new GameClearScene().push();
+                return true;
+            }
+        }
+        return false;
     }
 
     public void ResetWarriorAndTiles() {
         RemoveAllObjects();
-        this.tileGenerator.ResetGenerateObjects(warriorHead);
         warriorHead.ResetMove();
         warrior.SetAvailableChangeState();
+        if(!GameStateManager.getInstance().isTurnActive())
+            GameStateManager.getInstance().setTurnActive(true);
+
         this.resetTurn = false;
-        GameStateManager.getInstance().setTurnActive(false);
+
     }
 
     private void RemoveAllObjects(){

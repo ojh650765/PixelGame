fb50bf60c6147955c52b546d4bd49d90aa1a0d3d
JeongHun <45549589+ojh6507@users.noreply.github.com>
2024-06-10 01:00:03 +0900

    State 매니저에서 플레이어 정보들을 갖고, 이벤트 스테이지로부터 발생한 이벤트에서도 사용하자

--- Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/pixelgame/game/GameStateManager.java
+++ Project/app/src/main/java/kr/ac/tukorea/ge/spg/ojh/pixelgame/game/GameStateManager.java
@@ -3,6 +3,8 @@ package kr.ac.tukorea.ge.spg.ojh.pixelgame.game;
 import android.graphics.Canvas;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 
 import kr.ac.tukorea.ge.spg.ojh.framework.interfaces.IGameObject;
 import kr.ac.tukorea.ge.spg.ojh.framework.scene.Scene;
@@ -11,21 +13,35 @@ import kr.ac.tukorea.ge.spg.ojh.framework.util.CollisionHelper;
 public class GameStateManager implements IGameObject {
 
     private static GameStateManager instance;
-
+    private static final Random random = new Random();
     private  static float PlayerPower;
     private  static float PlayerDef;
 
     private boolean bisTurnActive;
+    private boolean isPause;
+    private int eventStageNum;
+    private boolean canIOpenEventStage;
+    private float PlayerHP;
+
+
     public GameStateManager () {
         PlayerPower = 1;
         PlayerDef = 0;
-        bisTurnActive = false;
+        PlayerHP = 100;
+        InitState(0);
     }
     public static GameStateManager getInstance() {
         if (instance == null) {
             instance = new GameStateManager();
         }
+
         return instance;
+    }
+    public  void InitState(int eventStageNum){
+        bisTurnActive = false;
+        isPause = false;
+        this.eventStageNum = eventStageNum;
+        this.canIOpenEventStage = random.nextBoolean();
     }
     public void UpdatePowerInfo(float p) {
         PlayerPower = p;
@@ -33,6 +49,13 @@ public class GameStateManager implements IGameObject {
     public void UpdateDefInfo(float d) {
         PlayerDef = d;
     }
+    public void UpdatePlayerHP(float hp) {
+        PlayerHP = hp;
+    }
+    public float GetPlayerHP(){
+        return PlayerHP;
+    }
+
     public float GetPowerInfo(){
         return  PlayerPower;
     }
@@ -45,6 +68,9 @@ public class GameStateManager implements IGameObject {
     public void setTurnActive(boolean turnActive) {
         bisTurnActive = turnActive;
     }
+    public void SetPauseState(boolean pause) {
+        isPause = pause;
+    }
     @Override
     public void update(float elapsedSeconds) {
 
@@ -53,4 +79,14 @@ public class GameStateManager implements IGameObject {
     public void draw(Canvas canvas) {
     }
 
+    public boolean GetisPause() {
+        return isPause;
+    }
+
+    public int GetEventStage() {
+        return eventStageNum;
+    }
+    public boolean GetEventStageCanOpen(){
+        return  canIOpenEventStage;
+    }
 }
